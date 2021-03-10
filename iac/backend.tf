bdd0f18fe7f94102a1f6407a086129c8d290a75f
Gab Contant <gcontant@gmail.com>
2021-03-09 19:58:55 -0500

    push change to trigger action

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "sagaorchestratorstorage"
-    container_name       = "testingterraform"
+    container_name       = "sagaterraform"
     key                  = "terraform.tfstate"
   }
 }
